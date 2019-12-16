5151631c2c17940105f449da2f9e299154e9709e
antilou86 <antilou86@gmail.com>
2019-12-16 16:52:31 -0500

    adding minor changes to doubly_linked_list.py changed an IS statement to a double equal sign.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -96,7 +96,7 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        if not is self.head:
+        if node is self.head:
             Return
         value = node.value
         self.delete(node)
@@ -117,13 +117,13 @@ class DoublyLinkedList:
         if not self.head and not self.tail:
             return
         self.length -= 1
-        if self.head is self.tail:
+        if self.head == self.tail:
             self.head = None
             self.tail = None
         elif self.head is node:
             self.head = node.next
             node.delete()
-        elif self. tail is node:
+        elif self.tail is node:
             self.tail = node.prev
             node.delete()
         else:
